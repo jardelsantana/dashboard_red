c7a11478574911487a737e683ef6bd01cd86c2f6
Jardel Santana <jardelsantana@users.noreply.github.com>
2024-02-05 17:52:32 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.write("""
 # Dashboard - Ativos da Redução
 """)
 
-df = pd.read_csv("ativos-reducao-tratados.csv")
+df = pd.read_csv("ativos-reducao-tratados.csv", index_col=0)
 
 num_of_rows = len(df)
 
