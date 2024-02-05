7f5f1c46d20dcb01045100d175304f27332c900f
Jardel Santana <jardelsantana@users.noreply.github.com>
2024-02-05 20:52:58 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,5 @@ df_asset_type = df[(df["AREA"] == area)]
 
 asset_types = df_asset_type["ASSET TYPE"].value_counts().index
 asset_type = st.sidebar.selectbox("Asset Types", asset_types)
+
+asset_status = df[df["ASSET TYPE"] == asset_type]
