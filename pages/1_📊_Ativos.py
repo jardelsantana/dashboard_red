dc7f5e65664a41fbb2e6b4fc4c421ac13686b179
Jardel Santana <jardelsantana@users.noreply.github.com>
2024-02-07 12:09:13 +0000

    add page

---
+++ pages/1_📊_Ativos.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+ 
+st.write("""
+# Dashboard - Ativos da Redução
+""")
+
+df = pd.read_csv("ativos-reducao-tratados.csv")
+
+areas = df["AREA"].value_counts().index
+area = st.sidebar.selectbox("Áreas", areas)
+df_asset_type = df[(df["AREA"] == area)]
+
+asset_types = df_asset_type["ASSET TYPE"].value_counts().index
+asset_type = st.sidebar.selectbox("Asset Types", asset_types)
+
+asset_status = df[df["ASSET TYPE"] == asset_type].set_index("ASSET")
+asset_status
