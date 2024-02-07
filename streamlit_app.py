86df422f4e52c93ef80d3001b18aa183dee0de11
Jardel Santana <jardelsantana@users.noreply.github.com>
2024-02-07 13:15:31 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,4 @@ st.write("""
 """)
 
 df = pd.read_csv("ativos-reducao-tratados.csv")
-
-areas = df["AREA"].value_counts().index
-area = st.sidebar.selectbox("Áreas", areas)
-df_asset_type = df[(df["AREA"] == area)]
-
-asset_types = df_asset_type["ASSET TYPE"].value_counts().index
-asset_type = st.sidebar.selectbox("Asset Types", asset_types)
-
-asset_status = df[df["ASSET TYPE"] == asset_type].set_index("ASSET")
-asset_status
+df
