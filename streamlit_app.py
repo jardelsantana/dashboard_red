109d5daca4521cf2a1ddefc2839f5f21c30bbf09
Jardel Santana <jardelsantana@users.noreply.github.com>
2024-02-08 13:37:42 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.write("""
 """)
 
 df = pd.read_csv("ativos-reducao-tratados.csv")
-df.set_index("ASSET",inplace=True )
+df.set_index("ASSET TYPE",inplace=True )
 
-st.bar_chart(df["ASSET TYPE"])
+st.bar_chart(df["ASSET"])
 df
