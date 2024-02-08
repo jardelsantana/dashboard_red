50edfad5defbf5fe0d90ae622c846872ea36032e
Jardel Santana <jardelsantana@users.noreply.github.com>
2024-02-08 13:41:42 -0300

    UPDATE

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ st.write("""
 df = pd.read_csv("ativos-reducao-tratados.csv")
 df.set_index("ASSET TYPE",inplace=True )
 
-st.area_chart(df["INSPECTION DATE"]).value_counts().index
+st.area_chart(df["INSPECTION DATE"])
 df
